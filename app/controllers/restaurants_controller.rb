dd83666ca2a4617d3ae3ac7a1c22b50654f40861
yunshengji <423450928@qq.com>
2018-08-01 17:00:42 +0800

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,49 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to restaurants_path
+  end
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+Restaurant.create!(name: "大龙易", address: "长寿路10号", phone_number: "18190842799", category: "chinese")
+Restaurant.create!(name: "小龙坎", address: "并将东路 10号", phone_number: "11021", category: "japanese")
+Restaurant.create!(name: "银杏", address: "并将东路 10号", phone_number: "11021", category: "french")
+Restaurant.create!(name: "疯狂烤翅", address: "东大街 10号", phone_number: "112", category: "italian")
+
